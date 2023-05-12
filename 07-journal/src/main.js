cfb25eb0560338f471e62372d5f993dc827e5ea7
Rodolfo Almaguer <rodoalmaguer@gmail.com>
2023-05-12 00:35:16 -0600

    Journal Vuex Module

--- 07-journal/src/main.js
+++ 07-journal/src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
+import './styles/styles.scss'
 import App from './App.vue'
 import router from './router'
-import './styles/styles.scss'
+import store from './store/index copy'
 
 createApp(App)
+    .use(store)
     .use(router)
     .mount('#app')
