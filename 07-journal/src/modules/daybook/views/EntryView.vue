b438d2020af0c3204d0aeff9c1653a811ec69660
Rodolfo Almaguer <rodoalmaguer@gmail.com>
2023-05-11 21:52:51 -0600

    EntryView

---
+++ 07-journal/src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="entry-title d-flex justify-content-between p-2">
+    <div>
+      <span class="text-success fs-3 fw-bold">11</span>
+      <span class="mx-1 fs-3">Mayo</span>
+      <span class="mx-2 fs-4 fw-light">2023, jueves</span>
+    </div>
+
+    <div>
+      <button class="btn btn-danger mx-2">
+        Borrar
+        <i class="fa fa-trash-alt"></i>
+      </button>
+
+      <button class="btn btn-primary">
+        Subir foto
+        <i class="fa fa-upload"></i>
+      </button>
+    </div>
+  </div>
+  <hr />
+  <div class="d-flex flex-column px-3 h-75">
+    <textarea placeholder="¿Qué sucedió hoy?"></textarea>
+  </div>
+  <FabJournal />
+  <img
+    src="https://www.robertlandscapes.com/wp-content/uploads/2014/11/landscape-322100_1280.jpg"
+    alt="entry-picture"
+    class="img-thumbnail"
+  />
+</template>
+<script>
+import { defineAsyncComponent } from "vue";
+export default {
+  components: {
+    FabJournal: defineAsyncComponent(() =>
+      import("../components/FabJournal.vue")
+    ),
+  },
+};
+</script>
